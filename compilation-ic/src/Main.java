e69f4ff0b04bf06df8564882ed8b51e38402c363
Karim Mahamed <mahamed.karim.15@gmail.com>
2017-03-07 22:07:20 +0200

    try/catch clause in the main method and minor refactors

--- compilation-ic/src/Main.java
+++ compilation-ic/src/Main.java
@@ -10,88 +10,61 @@ import IR.IR_Program;
 import SEMANTIC.SEMANTIC_SymbolTable;
 import java_cup.runtime.Symbol;
 
-public class Main
-{
-	public static final String OK_STRING = "OK";
-	public static final String FAIL_STRING = "FAIL";
+public class Main{
 	
-	static public void main(String argv[]) throws Exception
-	{
+	static public void main(String argv[]) throws Exception{
 		Lexer l;
 		Parser p;
 		FileReader file_reader;
-		String inputFileName = argv[0];
-		String outputFileName = argv[1];
-		String assemblyOutput = argv[1];
+		String inputICLocation = argv[0];
+		String assemblyFileLocation = argv[1];
 		
-		// TODO: Surround with try-catch
-	//	try
-	//	{
-			/********************************/
-			/* [1] Initialize a file reader */
-			/********************************/
-			file_reader = new FileReader(inputFileName);
+		try{
+			file_reader = new FileReader(inputICLocation);
 
-			/******************************/
-			/* [3] Initialize a new lexer */
-			/******************************/
 			l = new Lexer(file_reader);
-			
-			/*******************************/
-			/* [4] Initialize a new parser */
-			/*******************************/
+
 			p = new Parser(l);
+			
 			SEMANTIC_SymbolTable.hashTable.clear();
 			Symbol temp=p.parse();
 			AST_Program astNode = (AST_Program)temp.value;
-			if(astNode.validate(null)!=null)
-			{
-				// Writing output
-				BufferedWriter outputWriter = new BufferedWriter(new FileWriter(new File(outputFileName)));
-				outputWriter.write(OK_STRING);
-				outputWriter.close();
-				
+			if(astNode.validate(null)!=null){				
 				CODEGEN_AssemblyFilePrinter.reset();
-				FileWriter printer = CODEGEN_AssemblyFilePrinter.getInstance(assemblyOutput);
+				FileWriter printer = CODEGEN_AssemblyFilePrinter.getInstance(assemblyFileLocation);
 				CODEGEN_TemporaryFactory.reset();
-				// builds ir tree.
+				
 				SEMANTIC_SymbolTable.hashTable.clear();
 				CODEGEN_StringCollector.reset();
 				IR_Program irNode = astNode.createIR();
 				
-				// prints the strings before use them in the generate code.
 				CODEGEN_StringCollector.printStringsToAssembly();
 				
-				// prints vftables
 				CODEGEN_AssemblyFilePrinter.printVirtualFunctionsTablesAndUpdateVFTAdresses();
 				
 				CODEGEN_AssemblyFilePrinter.getInstance(null).write(String.format(".text%s", System.lineSeparator()));
-				// codeGen
+
 				irNode.generateCode();
 				printer.close();
 			}
-			else
-			{
-			// Writing output
-			BufferedWriter outputWriter = new BufferedWriter(new FileWriter(new File(outputFileName)));
-			outputWriter.write(FAIL_STRING);
-			outputWriter.close();
-			}
-			
-    /*	}     
-		catch (Exception e)
-		{
-			try
-			{
-				BufferedWriter outputWriter = new BufferedWriter(new FileWriter(new File(outputFileName)));
-				outputWriter.write(FAIL_STRING);
+			else{
+				BufferedWriter outputWriter = new BufferedWriter(new FileWriter(new File(assemblyFileLocation)));
+				outputWriter.write("FAIL");
 				outputWriter.close();
 			}
-			catch (Exception e2)
-			{
+			
+    	}     
+		catch (Exception e){
+			try{
+				BufferedWriter outputWriter = new BufferedWriter(new FileWriter(new File(assemblyFileLocation)));
+				outputWriter.write("FAIL");
+				outputWriter.close();
+			}
+			catch (Exception exiting){
+				System.out.println(String.format("Error: %s", exiting.getMessage()));
 			}
 		}
-		*/
+		
 	}
 }
 
